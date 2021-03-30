10e0707f21c04919c84c1ac2a6db42ead7637c97
Mark Oborne <markoborne1@gmail.com>
2021-03-30 16:10:26 +0100

    improved simd used packed_simd_2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use num_complex::Complex64;
 use sdl2::render::Canvas;
 use sdl2::{pixels, rect};
 use sdl2::video::Window;
 
-use std::arch::x86_64::*;
+use packed_simd_2::{f64x4, m64x4};
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct ComplexSIMD{
-    real: Vec4d,
-    imag: Vec4d,
+    real: f64x4,
+    imag: f64x4,
 }
 
 impl ComplexSIMD{
-    pub fn new(real: Vec4d, imag: Vec4d) -> Self{
+    pub fn new(real: f64x4, imag: f64x4) -> Self{
         Self{
             real,
             imag,
@@ -20,30 +19,7 @@ impl ComplexSIMD{
     }
 }
 
-#[derive(Clone)]
-struct Vec4d{
-    val: __m256d,
-}
-
-impl Vec4d{
-    pub unsafe fn new(val: f64) -> Self{
-        Self{
-            val: _mm256_set1_pd(val),
-        }
-    }
-    pub unsafe fn from(val1: f64, val2: f64, val3: f64, val4: f64) -> Self{
-        Self{
-            val: _mm256_setr_pd(val1, val2, val3, val4),
-        }
-    }
-    pub unsafe fn take(val: __m256d) -> Self{
-        Self{
-            val,
-        }
-    }
-}
-
-impl std::ops::Add for Vec4d{
+/*impl std::ops::Add for Vec4d{
     type Output = Self;
     fn add(self, other: Self) -> Self::Output{
         Self{
@@ -66,97 +42,59 @@ impl std::ops::Sub for Vec4d{
             val: unsafe { _mm256_sub_pd(self.val, other.val) },
         }
     }
-}
+}*/
 
-#[cfg(target_feature = "avx2")]
-unsafe fn mandelbrot(z: ComplexSIMD, c: ComplexSIMD) -> ComplexSIMD{
-    let nr = z.real.clone() * z.real.clone() - z.imag.clone() * z.imag.clone() + c.real.clone();
-    let ni = z.real.clone() * z.imag.clone() * Vec4d::new(2.0) + c.imag.clone();
+//#[cfg(target_feature = "avx2")]
+fn mandelbrot(z: ComplexSIMD, c: ComplexSIMD) -> ComplexSIMD{
+    let nr = z.real * z.real - z.imag * z.imag + c.real;
+    let ni = z.real * z.imag * 2.0 + c.imag;
     ComplexSIMD::new(nr, ni)
 }
 
-#[cfg(target_feature = "avx2")]
-unsafe fn escape_check(z: ComplexSIMD) -> Vec4d{
-    Vec4d::take(_mm256_cmp_pd((z.real.clone() * z.real.clone() + z.imag.clone() * z.imag.clone()).val,
-                  _mm256_set1_pd(4.0), _CMP_GT_OQ))
-}
-
-#[cfg(target_feature = "avx2")]
-unsafe fn compute_point(c: ComplexSIMD) -> Vec4d{// TODO: simd check if changed, simd check if higher?
-    const MAX_ITER: i32 = 255 * 3;
-    let mut z = ComplexSIMD::new(Vec4d::new(0.0), Vec4d::new(0.0));
-    let mut result: [i32; 4] = [MAX_ITER, MAX_ITER, MAX_ITER, MAX_ITER];
-    let mut r = [false, false, false, false];
-    for i in 1..=MAX_ITER {
-        z = mandelbrot(z, c.clone());
-        let iters = escape_check(z.clone());
-        simd_extract(iters.clone(), 0);
-        if iters[0] && !r[0] {
-            result[0] = i;
-            r[0] = true;
-        }
-        if iters[1] && !r[1] {
-            result[1] = i;
-            r[1] = true;
-        }
-        if iters[2] && !r[2] {
-            result[2] = i;
-            r[2] = true;
-        }
-        if iters[3] && !r[3] {
-            result[3] = i;
-            r[3] = true;
-        }
-        if r[0] && r[1] && r[2] && r[3] {
-            Vec4d::from(result[0] as f64, result[1] as f64,
-                               result[2] as f64, result[3] as f64)
-        }
-    }
-    Vec4d::from(result[0] as f64, result[1] as f64, result[2] as f64, result[3] as f64)
-}
-
-
-/// calculates the quadratic map for the Mandelbrot set
-fn mandelbrot(z: Complex64, c: Complex64) -> Complex64{
-    z * z + c
-}
-
-fn escape_check(z: Complex64) -> bool{
-    (z.re * z.re + z.im * z.im) > 4.0
-}
-
-/// calculates the number of iterations until it takes to escape
-fn compute_point(c: Complex64) -> i32{
-    const MAX_ITER: i32 = 255 * 3;
-    let mut z = Complex64::new(0.0, 0.0);
-    for i in 1..MAX_ITER{
-        z = mandelbrot(z, c);
-        if escape_check(z) {
-            return i
-        }
-    }
-    MAX_ITER
+//#[cfg(target_feature = "avx2")]
+fn escape_check(z: ComplexSIMD) -> m64x4{
+    (z.real * z.real + z.imag * z.imag).gt(f64x4::splat(4.0))
 }
 
 /// Converts the x y coordinates of the pixels to the mandelbrot coordinates
-fn pixel_to_mandelbrot(pixel: i32, offset: f64, zoom: f64, zoom_correction: i32) -> f64{
-    (pixel - zoom_correction) as f64 * zoom + offset
+fn pixel_to_mandelbrot(pixel: f64x4, offset: f64, zoom: f64, zoom_correction: i32) -> f64x4{
+    (pixel - zoom_correction as f64) * zoom + offset
 }
 
 /// renders the mandelbrot set to a SDL2 canvas
 fn render_mandelbrot(canvas: &mut Canvas<Window>, x_offset: f64, y_offset: f64, zoom: f64, width: i32, height: i32){
     canvas.set_draw_color(pixels::Color::RGB(30, 30, 30));
     canvas.clear();
+    const MAX_ITER: i32 = 255 * 3;
     let width_correction = width / 2; // makes the zoom center on the middle of the camera
     let height_correction = height / 2;
-    for i in 0..width{
-        for j in 0..height{
-            let iterations = compute_point(Complex64::new(
-                pixel_to_mandelbrot(i, x_offset, zoom, width_correction),
-                pixel_to_mandelbrot(j, y_offset, zoom, height_correction)));
-            let colour = (iterations % 255) as u8;
-            canvas.set_draw_color(pixels::Color::RGB(colour, colour, colour));
-            canvas.fill_rect(rect::Rect::new(i, j, 1, 1)).expect("Failed to create rectangle");
+    for i in 0..height{
+        for j in (0..width).step_by(4){
+            let c= ComplexSIMD::new(pixel_to_mandelbrot(f64x4::new(j as f64, j as f64 + 1.0, j as f64 + 2.0,
+                                                                   j as f64 + 3.0), x_offset, zoom, width_correction),
+                pixel_to_mandelbrot(f64x4::splat(i as f64), y_offset, zoom, height_correction));
+            let mut num_of_iters = f64x4::splat(0.0);
+            let mut z = ComplexSIMD::new(f64x4::splat(0.0), f64x4::splat(0.0));
+            
+            'esc: for _ in 0..=MAX_ITER {
+                z = mandelbrot(z, c);
+                let escape_mask = escape_check(z);
+                if escape_mask.all() {
+                    break 'esc;
+                }
+                let a = f64x4::new(if escape_mask.extract(0) {1.0} else {0.0}, // todo: scuffed, change
+                                   if escape_mask.extract(1) {1.0} else {0.0},
+                                   if escape_mask.extract(2) {1.0} else {0.0},
+                                   if escape_mask.extract(3) {1.0} else {0.0});
+                num_of_iters += a;
+            }
+            let colour = (num_of_iters % 3.0);
+            for k in 0..4 {
+                canvas.set_draw_color(pixels::Color::RGB(colour.extract(k) as u8, colour.extract(k) as u8, 
+                                                         colour.extract(k) as u8));
+                canvas.fill_rect(rect::Rect::new(i, j + k as i32, 1, 1))
+                    .expect("Failed to create rectangle");
+            }
         }
     }
 }
@@ -168,7 +106,6 @@ fn main() {
     let sdl_context = sdl2::init().expect("Failed to create sdl context");
     let video_subsystem = sdl_context.video().expect("Failed to create video subsystem");
     let window = video_subsystem.window("Mandelbrot set", WIDTH as u32, HEIGHT as u32)
-        .fullscreen()
         .vulkan()
         .build()
         .expect("Failed to create window");
